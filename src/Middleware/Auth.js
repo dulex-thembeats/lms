f8596f9b9f87da2b32c8d2adf8ed1a2d2ac16426
Muhammad <dulex51@hotmail.com>
2021-01-28 23:15:36 +0100

    hopefull

--- src/Middleware/Auth.js
+++ src/Middleware/Auth.js
@@ -1,21 +1,21 @@
-const student = require("../models/student");
+const students = require("../models/student");
 const jwt = require("jsonwebtoken");
 
 const auth = async (req, res, next) => {
   try {
     const token = req.header("Authorization").replace("Bearer ", "");
     const decoded = jwt.verify(token, "thisismytoken");
-    const Student = await student.findOne({
+    const student = await students.findOne({
       _id: decoded._id,
       "tokens.token": token,
     });
 
-    if (!Student) {
+    if (!student) {
       throw new Error();
     }
 
     req.token = token;
-    req.User = Student;
+    req.User = student;
     next();
   } catch (e) {
     res.status(401).send({ error: "Please Authenticate" });
